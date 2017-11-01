c7ad43d279d4795cbcdaa7d22609010b6c5124b1
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-11-01 22:12:45 +0530

    Added RepeatAndMissingNumberArray.cpp

---
+++ Arrays/RepeatAndMissingNumberArray.cpp
@@ -0,0 +1,52 @@
+/*
+
+You are given a read only array of n integers from 1 to n.
+
+Each integer appears exactly once except A which appears twice and B which is missing.
+
+Return A and B.
+
+Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Note that in your output A should precede B.
+
+Example:
+
+Input:[3 1 2 5 3] 
+
+Output:[3, 4] 
+
+A = 3, B = 4
+
+*/
+
+
+
+vector<int> Solution::repeatedNumber(const vector<int> &A)
+{
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    vector<int> res;
+    long long n=A.size(), x=0, y=0;
+    
+    long long sum=(n*(n+1))/2;
+    
+    long long sumSq=(n*(n+1)*(2*n+1))/6;
+    
+    for(int i=0;i<n;i++)
+    {
+        sum-=(long long)A[i];
+        sumSq-=(long long)A[i]*(long long)A[i];
+    }
+    
+    y=(sum+sumSq/sum)/2;
+    x=y-sum;
+    
+    res.push_back(x); res.push_back(y);
+    return res;
+}
+
+
