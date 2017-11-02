c116a186c1daeafecd388c18d8f0d560620e0337
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-11-02 11:28:46 +0530

    Updated MaximumAbsoluteDifference.cpp

--- Arrays/MaximumAbsoluteDifference.cpp
+++ Arrays/MaximumAbsoluteDifference.cpp
@@ -1,5 +1,7 @@
 /*
 
+https://www.interviewbit.com/problems/Maximum-Absolute-Difference/
+
 You are given an array of N integers, A1, A2 ,…, AN. Return maximum value of f(i, j) for all 1 ≤ i, j ≤ N.
 f(i, j) is defined as |A[i] - A[j]| + |i - j|, where |x| denotes absolute value of x.
 
