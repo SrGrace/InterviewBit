b145ff0aeaa3671acc2aeb2b8dcfb597d64ad76b
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-11-02 11:31:43 +0530

    Updated SetMatrixZeros.cpp

--- Arrays/SetMatrixZeros.cpp
+++ Arrays/SetMatrixZeros.cpp
@@ -1,5 +1,7 @@
 /*
 
+https://www.interviewbit.com/problems/Set-Matrix-Zeros/
+
 
 
 Given an m x n matrix of 0s and 1s, if an element is 0, set its entire row and column to 0.
@@ -24,7 +26,8 @@ Note that this will be evaluated on the extra memory used. Try to minimize the s
 
 */
 
-void Solution::setZeroes(vector<vector<int> > &matrix) {
+void Solution::setZeroes(vector<vector<int> > &matrix) 
+{
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
@@ -34,19 +37,28 @@ void Solution::setZeroes(vector<vector<int> > &matrix) {
     
     int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
 
-    for (int i = 0; i < rows; i++) {
+    for (int i = 0; i < rows; i++) 
+    {
+        
         if (matrix[i][0] == 0) col0 = 0;
+        
         for (int j = 1; j < cols; j++)
             if (matrix[i][j] == 0)
                 matrix[i][0] = matrix[0][j] = 0;
     }
 
-    for (int i = rows - 1; i >= 0; i--) {
+    for (int i = rows - 1; i >= 0; i--) 
+    {
         for (int j = cols - 1; j >= 1; j--)
             if (matrix[i][0] == 0 || matrix[0][j] == 0)
                 matrix[i][j] = 0;
+        
         if (col0 == 0) matrix[i][0] = 0;
     }
     
 }
+
+
+
+
 
