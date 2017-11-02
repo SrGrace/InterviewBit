c5a06cae1a1292c8993ecceaa3cae9045d7f835c
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-11-02 11:48:44 +0530

    Updated MergeKSortedLists.cpp

--- Heaps And Maps/MergeKSortedLists.cpp
+++ Heaps And Maps/MergeKSortedLists.cpp
@@ -31,6 +31,9 @@ will result in
  * };
  */
  
+
+# 1st method
+
  
 struct cmpr {
     bool operator()(const ListNode* l, const ListNode* r) {
@@ -70,7 +73,50 @@ ListNode* Solution::mergeKLists(vector<ListNode*> &A)
 
 
 
+# 2nd method
+
+# The basic idea is really simple. We can merge first two lists and then push it back. 
+# Keep doing this until there is only one list left in vector. Actually,
+# we can regard this as an iterative divide-and-conquer solution.
 
 
+ListNode *merge2lists(ListNode *l1, ListNode *l2)
+{
+    if(l1==nullptr) return l2;
+    if(l2==nullptr) return l1;
+ 
+    if(l1->val <= l2->val)
+    {
+       l1->next = merge2lists(l1->next, l2);
+       return l1;
+    }
+ 
+    else
+    {
+       l2->next = merge2lists(l2->next, l1);
+       return l2;      
+    }
+}
 
+
+ListNode* Solution::mergeKLists(vector<ListNode*> &A) 
+{
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    if(A.size() == 0) return nullptr;
+ 
+    while(A.size() > 1)
+    {
+        A.push_back(merge2lists(A[0], A[1]));
+        A.erase(A.begin());
+        A.erase(A.begin());
+    }
+ 
+    return A.front();
+ 
+ 
+}
 
