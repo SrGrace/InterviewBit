3b4277bac7216731e764835bd911ea29dc8a6d28
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-11-06 15:31:07 +0530

    Create Checkpoint:Level2Prettyprint.cpp

---
+++ CheckpointProblems/Checkpoint:Level2Prettyprint.cpp
@@ -0,0 +1,77 @@
+/*
+
+https://www.interviewbit.com/problems/prettyprint/
+
+
+Print concentric rectangular pattern in a 2d matrix.
+Let us show you some examples to clarify what we mean.
+
+Example 1:
+
+Input: A = 4.
+Output:
+
+4 4 4 4 4 4 4 
+4 3 3 3 3 3 4 
+4 3 2 2 2 3 4 
+4 3 2 1 2 3 4 
+4 3 2 2 2 3 4 
+4 3 3 3 3 3 4 
+4 4 4 4 4 4 4 
+
+Example 2:
+
+Input: A = 3.
+Output:
+
+3 3 3 3 3 
+3 2 2 2 3 
+3 2 1 2 3 
+3 2 2 2 3 
+3 3 3 3 3 
+
+The outermost rectangle is formed by A, then the next outermost is formed by A-1 and so on.
+
+You will be given A as an argument to the function you need to implement, and you need to return a 2D array.
+
+*/
+
+
+
+
+
+vector<vector<int> > Solution::prettyPrint(int A) 
+{
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    int n = 2*A-1;
+    int lim = n, tmp = 0;
+    
+    vector<vector<int> > v(n, vector<int> (n));
+    
+    while(A > 0)
+    {
+        for(auto i=tmp; i<lim; i++)
+        {
+            for(auto j=tmp; j<lim; j++)
+            {
+                v[i][j] = A;
+            }
+        }
+        
+        A--;
+        lim--;
+        tmp++;
+    }
+    
+    return v;
+    
+}
+
+
+
+
+
