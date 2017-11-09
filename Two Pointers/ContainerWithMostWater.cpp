d512757cd1d6325c374f8e1879af67fc8aa83588
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-11-09 19:39:13 +0530

    Create ContainerWithMostWater.cpp

---
+++ Two Pointers/ContainerWithMostWater.cpp
@@ -0,0 +1,51 @@
+/*
+
+https://www.interviewbit.com/problems/container-with-most-water/
+
+
+Given n non-negative integers a1, a2, ..., an,
+where each represents a point at coordinate (i, ai).
+'n' vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+
+Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Your program should return an integer which corresponds to the maximum area of water that can be contained ( Yes, we know maximum area instead of maximum volume sounds weird. But this is 2D plane we are working with for simplicity ).
+
+    Note: You may not slant the container. 
+
+Example :
+
+Input : [1, 5, 4, 3]
+Output : 6
+
+Explanation : 5 and 3 are distance 2 apart. So size of the base = 2. Height of container = min(5, 3) = 3. 
+So total area = 3 * 2 = 6
+
+*/
+
+
+int Solution::maxArea(vector<int> &A) 
+{
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    int maxArea = 0, l = 0, r = A.size() - 1;
+    
+    while(l < r)
+    {
+        maxArea = max(maxArea, min(A[l], A[r])*(r-l));
+        
+        if(A[l] < A[r])
+            l++;
+        else
+            r--;
+    }
+    return maxArea;
+}
+
+
+
+
+
