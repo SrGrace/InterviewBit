c28384deaccd833eb52f865502ed3d2a95b2cea1
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-11-09 23:34:05 +0530

    Create K-ReverseLinkedList.cpp

---
+++ Linked Lists/K-ReverseLinkedList.cpp
@@ -0,0 +1,49 @@
+/*
+
+
+Given a singly linked list and an integer K, reverses the nodes of the
+
+list K at a time and returns modified linked list.
+
+    NOTE : The length of the list is divisible by K 
+
+Example :
+
+Given linked list 1 -> 2 -> 3 -> 4 -> 5 -> 6 and K=2,
+
+You should return 2 -> 1 -> 4 -> 3 -> 6 -> 5
+
+Try to solve the problem using constant extra space.
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::reverseList(ListNode* A, int B) 
+{
+    ListNode *nxt = NULL, *tmp = NULL, *curr = A;
+    int cnt = 0;
+    
+    while(curr && cnt < B)
+    {
+        nxt = curr -> next;
+        curr -> next = tmp;
+        tmp = curr;
+        curr = nxt;
+        
+        cnt++;
+    }
+    if(nxt)
+        A -> next = reverseList(nxt, B);
+        
+    return tmp;
+}
+
+
