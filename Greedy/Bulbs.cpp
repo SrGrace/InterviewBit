2e5f282ece20d7714795323c97b79945a0919f4a
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-11-27 11:06:52 +0530

    Create Bulbs.cpp

---
+++ Greedy/Bulbs.cpp
@@ -0,0 +1,40 @@
+/*
+
+https://www.interviewbit.com/problems/bulbs/
+
+
+N light bulbs are connected by a wire. Each bulb has a switch associated with it, however due to faulty wiring, a switch also changes the state of all the bulbs to the right of current bulb. Given an initial state of all bulbs, find the minimum number of switches you have to press to turn on all the bulbs. You can press the same switch multiple times.
+
+Note : 0 represents the bulb is off and 1 represents the bulb is on.
+
+Example:
+
+Input : [0 1 0 1]
+Return : 4
+
+Explanation :
+	press switch 0 : [1 0 1 0]
+	press switch 1 : [1 1 0 1]
+	press switch 2 : [1 1 1 0]
+	press switch 3 : [1 1 1 1]
+
+*/
+
+
+int Solution::bulbs(vector<int> &A) 
+{
+    int state = 0, cnt = 0;
+    
+    for(int i=0; i<A.size(); i++)
+    {
+        if(A[i] == state)
+        {
+            cnt++;
+            state = !state;
+        }
+        
+    }
+    return cnt;
+}
+
+
