219eabcda1623e1fe4f48af280deb3530e6249f3
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-12-11 22:13:17 +0530

    Update HotelBookingsPossible.cpp

--- Arrays/HotelBookingsPossible.cpp
+++ Arrays/HotelBookingsPossible.cpp
@@ -34,7 +34,45 @@ At day = 5, there are 2 guests in the hotel. But I have only one room.
 */
 
 
+# 1st method
 
+
+class Solution {
+    public:
+    
+    bool hotel(vector<int> &arrive, vector<int> &depart, int K) {
+        
+        if(K == 0)
+                return false;
+
+        int N = arrive.size();
+        
+        vector<pair<int, int> > vec;
+        for(int i = 0; i < N; ++i) {
+            vec.push_back(make_pair(arrive[i], 1));
+            vec.push_back(make_pair(depart[i], 0));
+        }
+         
+        sort(vec.begin(), vec.end());
+        
+        int curActive = 0;
+        int maxAns = 0;
+        for (int i = 0; i < vec.size(); i++) {
+           if (vec[i].second == 1) { // arrival
+               curActive++;
+               maxAns = max(maxAns, curActive);
+            } else {
+                curActive--;
+            }
+        }
+
+        return (K >= maxAns);
+    }
+};
+
+
+
+# 2nd method
 
 
 bool Solution::hotel(vector<int> &arrive, vector<int> &depart, int K) 
@@ -62,6 +100,5 @@ bool Solution::hotel(vector<int> &arrive, vector<int> &depart, int K)
     return (K >= room);
     
 }
-
 
 
