50efcc96cbbb5cbd53a60c6429f04153929faec2
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-12-11 21:58:17 +0530

    Update Flip.cpp

--- Arrays/Flip.cpp
+++ Arrays/Flip.cpp
@@ -3,9 +3,13 @@
 https://www.interviewbit.com/problems/flip/
 
 
-You are given a binary string(i.e. with characters 0 and 1) S consisting of characters S1, S2, …, SN. In a single operation, you can choose two indices L and R such that 1 ≤ L ≤ R ≤ N and flip the characters SL, SL+1, …, SR. By flipping, we mean change character 0 to 1 and vice-versa.
+You are given a binary string(i.e. with characters 0 and 1) S consisting of characters S1, S2, …, SN. In a single operation, you can 
+choose two indices L and R such that 1 ≤ L ≤ R ≤ N and flip the characters SL, SL+1, …, SR. By flipping, we mean change character 0 
+to 1 and vice-versa.
 
-Your aim is to perform ATMOST one operation such that in final string number of 1s is maximised. If you don’t want to perform the operation, return an empty array. Else, return an array consisting of two elements denoting L and R. If there are multiple solutions, return the lexicographically smallest pair of L and R.
+Your aim is to perform ATMOST one operation such that in final string number of 1s is maximised. If you don’t want to perform the 
+operation, return an empty array. Else, return an array consisting of two elements denoting L and R. If there are multiple solutions, 
+return the lexicographically smallest pair of L and R.
 
 Notes:
 
@@ -73,8 +77,6 @@ vector<int> Solution::flip(string A)
     
     return res;
 }
-
-
 
 
 
