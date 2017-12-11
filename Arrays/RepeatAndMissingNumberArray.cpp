83791de40495bfa50db162e14cd24f5f98e0d8c3
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-12-11 23:15:54 +0530

    Update RepeatAndMissingNumberArray.cpp

--- Arrays/RepeatAndMissingNumberArray.cpp
+++ Arrays/RepeatAndMissingNumberArray.cpp
@@ -23,6 +23,21 @@ A = 3, B = 4
 
 */
 
+# Approach
+
+/*
+
+    Sum(Actual) = Sum(1...N) + A - B
+
+    Sum(Actual) - Sum(1...N) = A - B. 
+
+    Sum(Actual Squares) = Sum(1^2 ... N^2) + A^2 - B^2
+
+    Sum(Actual Squares) - Sum(1^2 ... N^2) = (A - B)(A + B) = (Sum(Actual) - Sum(1...N)) ( A + B). 
+    
+We can use the above 2 equations to get the value of A and B.
+
+*/
 
 
 vector<int> Solution::repeatedNumber(const vector<int> &A)
