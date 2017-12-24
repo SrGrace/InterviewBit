f29b7308a97c20af14da508d20539cd2365c95c5
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-12-24 19:06:33 +0530

    Update FindDuplicateInArray.cpp

--- Arrays/FindDuplicateInArray.cpp
+++ Arrays/FindDuplicateInArray.cpp
@@ -4,7 +4,8 @@ https://www.interviewbit.com/problems/find-duplicate-in-array/
 
 
 
-Given a read only array of n + 1 integers between 1 and n, find one number that repeats in linear time using less than O(n) space and traversing the stream sequentially O(1) times.
+Given a read only array of n + 1 integers between 1 and n, find one number that repeats in linear time using less than O(n) space 
+and traversing the stream sequentially O(1) times.
 
 Sample Input:
 
