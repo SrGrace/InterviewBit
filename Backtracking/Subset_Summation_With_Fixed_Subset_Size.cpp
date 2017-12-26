10a8134003f00a5b7926d805dae1283d8829ada2
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-12-26 16:00:20 +0530

    Create Subset_Summation_With_Fixed_Subset_Size.cpp

---
+++ Backtracking/Subset_Summation_With_Fixed_Subset_Size.cpp
@@ -0,0 +1,35 @@
+
+
+/*
+
+Write a function to determine if it is possible to choose a subset of elements from a given array such that the sum of all 
+numbers in the subset is equal to a given target.
+
+Assume target is always a positive number.
+
+Examples: 
+
+group_sum({1,2,3,6,5}, 5, 2, 10) ==> true
+group_sum({1,2,3,6,5}, 5, 3, 18) ==> false
+
+
+Array: [Empty], Size: 0
+must_have number: 3
+Target Sum: 7                                                  false
+
+Array: {1,2,3,6,5}, Size: 5
+must_have number: 3
+Target Sum: 7                                                   false
+
+Array: {4,6,7,9,10,-11}, Size: 6
+must_have number: 6
+Target Sum: 12                                                   true
+
+*/
+
+
+
+bool Subset_Sum_With_Fixed_Subset_Size(int arr[], int sz, int must_have, int target_sum)
+{
+
+}
