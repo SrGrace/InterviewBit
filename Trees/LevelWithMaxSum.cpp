f89db70c0f6d067e7a61a62884b9c41527c57de6
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-12-26 15:48:31 +0530

    Update LevelWithMaxSum.cpp

--- Trees/LevelWithMaxSum.cpp
+++ Trees/LevelWithMaxSum.cpp
@@ -1,7 +1,45 @@
 
 
-treeNode *level_with_max_sum(treeNode *root)
+int level_with_max_sum(treeNode *root)
 {
-
+    if(!root)
+        return -1;
+      
+    std::queue<treeNode*>q ;
+    q.push(root);
+    
+    int sum = 0, max_sum = 0, level = 0, max_level = 0;    
+    while(!q.empty())
+    {
+        treeNode *tmp = q.front();
+        q.pop();
+        
+        if(!tmp)
+        {
+            if(sum > max_sum)
+            {
+                max_sum  = sum;
+                max_level = level;
+            }
+            sum = 0;
+    
+            if(!q.empty())
+                q.push(NULL);
+            level++;
+        }
+        else
+        {
+            sum += tmp->value;
+            if(tmp->left)
+                q.push(tmp->left);
+            if(tmp->right)
+                q.push(tmp->right);
+        }
+    }
+    return max_level;
 
 }
+
+
+
+
