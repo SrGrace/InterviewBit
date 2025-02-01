52074db905b0482db54cffc6430e61faf554e70a
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2025-02-01 16:58:42 +0530

    Update DistinceNumbersInWindow.py

--- Heaps And Maps/DistinceNumbersInWindow.py
+++ Heaps And Maps/DistinceNumbersInWindow.py
@@ -1,3 +1,33 @@
+"""
+
+https://www.interviewbit.com/problems/distinct-numbers-in-window/
+
+
+You are given an array of N integers, A1, A2 ,…, AN and an integer K. Return the of count of distinct numbers in all windows of size K.
+
+Formally, return an array of size N-K+1 where i’th element in this array contains number of distinct elements in sequence 
+Ai, Ai+1 ,…, Ai+k-1.
+
+Note:
+
+    If K > N, return empty array.
+
+For example,
+
+A=[1, 2, 1, 3, 4, 3] and K = 3
+
+All windows of size K are
+
+[1, 2, 1]
+[2, 1, 3]
+[1, 3, 4]
+[3, 4, 3]
+
+So, we return an array [2, 3, 3, 2].
+
+
+"""
+
 class Solution:
 	# @param A : list of integers
 	# @param B : integer
