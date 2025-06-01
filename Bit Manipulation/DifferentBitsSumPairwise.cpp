cccb660a5570128e4f4c0b4e9aebcb58e97e780a
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2025-06-01 19:56:45 +0530

    Update DifferentBitsSumPairwise.cpp

--- Bit Manipulation/DifferentBitsSumPairwise.cpp
+++ Bit Manipulation/DifferentBitsSumPairwise.cpp
@@ -3,8 +3,8 @@
 https://www.interviewbit.com/problems/different-bits-sum-pairwise/
 
 
-
-We define f(X, Y) as number of different corresponding bits in binary representation of X and Y. For example, f(2, 7) = 2, since binary representation of 2 and 7 are 010 and 111, respectively. The first and the third bit differ, so f(2, 7) = 2.
+We define f(X, Y) as number of different corresponding bits in binary representation of X and Y. For example, f(2, 7) = 2, 
+since binary representation of 2 and 7 are 010 and 111, respectively. The first and the third bit differ, so f(2, 7) = 2.
 
 You are given an array of N positive integers, A1, A2 ,…, AN. Find sum of f(Ai, Aj) for all pairs (i, j) such that 1 ≤ i, j ≤ N. Return the answer modulo 109+7.
 
@@ -25,6 +25,7 @@ f(5, 1) + f(5, 3) + f(5, 5) =
 */
 
 
+/*
 # solution Approach:
 
 An Efficient Solution can solve this problem in O(n) time using the fact that all numbers are represented 
@@ -32,6 +33,7 @@ using 32 bits (or some fixed number of bits). The idea is to count differences a
 We traverse from 0 to 31 and count numbers with i’th bit set. Let this count be ‘count’. There would be “n-count” numbers 
 with i’th bit not set. So count of differences at i’th bit would be “count * (n-count) * 2”. 
 
+*/
 
 
 
